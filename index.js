3c27250ea5c119343b281e70af7781e28882d9d0
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-12 18:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map ( function (driver) { return ` ${driver.name} is from ${driver.hometown}`}) }
+  return drivers.map ( function (driver) { return ` ${driver.name} is from ${driver.hometown}`})
+}
